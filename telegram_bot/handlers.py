16d714e7137b5e28cdbd7cd5fcb70e998e3ef7db
nikkov199525 <D:\The_Bat_portable_7.4.6_Zorro\Data\Mail>
2025-04-16 09:56:50 +0300

    small fixes

--- telegram_bot/handlers.py
+++ telegram_bot/handlers.py
@@ -6,7 +6,5 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Добро пожаловать! Я ваш помощник по финансовому планированию...")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    commands_text = ("Доступные команды:\n"
-                     "/start - Приветствие\n"
-                     "/help - Список команд")
+    commands_text = ("Доступные команды:\n /start - Приветствие\n /help - Список команд")
     await update.message.reply_text(commands_text)
