ff426cd9e16501725892b77e68d5701b87a9801a
nikitachuev2 <chuev3@bk.ru>
2025-04-23 10:58:56 +0300

    bot

--- my_dreams_site/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
